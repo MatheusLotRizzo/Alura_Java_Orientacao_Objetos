6de8e1ca0f2911670aaf9dc7c88cc6f46c6d2b50
Matheus Rizzo <Matheus Rizzo@LECOM-01871>
2021-05-11 18:29:38 -0300

    Realizado a pratica de herança e polimorfismo durante a aula

--- byteBank/src/HerencaAndConstrutores/TestesConta.java
+++ byteBank/src/HerencaAndConstrutores/TestesConta.java
@@ -3,9 +3,11 @@ package HerencaAndConstrutores;
 public class TestesConta {
 
 	public static void main(String[] args) {
-		ContaCorrente cc = new ContaCorrente(9125, 9922);
-		cc.deposita(500);
 		
+		ContaCorrente cc = new ContaCorrente(9125, 9922);
+		cc.deposita(100);
+		
+		/*
 		ContaPoupanca cp = new ContaPoupanca(9125, 9923);
 		cp.deposita(200);
 		
@@ -15,7 +17,13 @@ public class TestesConta {
 		System.out.println("cp: "+cp.getSaldo());
 		
 		cc.saca(9);
-		System.out.println(cc.getSaldo());
+		System.out.println(cc.getSaldo());*/
+		
+		SeguroVida seguro = new SeguroVida();
+		CalculadorImposto imposto = new CalculadorImposto();
+		imposto.registra(cc);
+		imposto.registra(seguro);
+		System.out.println(imposto.getValorImposto());
 	}
 
 }
