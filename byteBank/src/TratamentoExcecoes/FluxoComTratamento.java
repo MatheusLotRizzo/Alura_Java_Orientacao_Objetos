40776e4bd52713be7cdd31aaf9640d88c71b75e0
Matheus Rizzo <Matheus Rizzo@192.168.50.41>
2021-05-13 20:37:57 -0300

    Lançando exceções

--- byteBank/src/TratamentoExcecoes/FluxoComTratamento.java
+++ byteBank/src/TratamentoExcecoes/FluxoComTratamento.java
@@ -1,6 +1,6 @@
 package TratamentoExcecoes;
 
-public class Fluxo {
+public class FluxoComTratamento {
 
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -15,19 +15,26 @@ public class Fluxo {
             metodo2();
         }catch(ArithmeticException | NullPointerException ex) {
         	String msg = ex.getMessage();
-        	System.out.println("Exception "+msg);
+        	System.out.println("Exceção"+msg);
         	ex.printStackTrace();
-        }
+        }/*catch(NullPointerException ex) {
+        	ex.printStackTrace();
+        }*/catch (Exception ex) {
+			
+		}
         
         System.out.println("Fim do metodo1");
     }
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        
-        throw new ArithmeticException("deu ruim");
-        
-        //System.out.println("Fim do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            //int a = i/0;
+            Conta c = null;
+            c.deposita();
+        }
+        System.out.println("Fim do metodo2");
     }
 }
 
